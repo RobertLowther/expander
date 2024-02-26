b40845aa69fdff0ec45d7d2f36071243df963b51
Robert Lowther <drail159@gmail.com>
2024-02-25 23:14:48 -0700

    respond to shift key

--- src/keylogger.rs
+++ src/keylogger.rs
@@ -1,38 +1,136 @@
-use device_query::{DeviceQuery, DeviceState};
+use device_query::{DeviceQuery, DeviceState, Keycode};
+use std::collections::HashMap;
 
-// let mut prev_ctrl_keys: Vec<device_query::Keycode> = vec![];
-// let mut prev_data_keys: Vec<device_query::Keycode> = vec![];
-
-/// Keylogger launch function
-pub fn run() -> std::collections::HashMap<String, Vec<device_query::Keycode>> {
+pub fn run() -> std::collections::HashMap<String, Vec<String>> {
     let device_state = DeviceState::new();
     
     let ctrl_keycodes = vec![
-        device_query::Keycode::LShift,
-        device_query::Keycode::RShift,
-        device_query::Keycode::LControl,
-        device_query::Keycode::RControl,
-        device_query::Keycode::LAlt,
-        device_query::Keycode::RAlt,
-        device_query::Keycode::Escape,
+        Keycode::LShift,
+        Keycode::RShift,
+        Keycode::LControl,
+        Keycode::RControl,
+        Keycode::LAlt,
+        Keycode::RAlt,
+        Keycode::Escape,
     ];
 
+    let shift_keycodes = vec![
+        Keycode::LShift,
+        Keycode::RShift,
+    ];
+
+    let mut letter_shift_map: HashMap<Keycode, String> = HashMap::new();
+    letter_shift_map.insert(Keycode::A, "a".to_string());
+    letter_shift_map.insert(Keycode::B, "b".to_string());
+    letter_shift_map.insert(Keycode::C, "c".to_string());
+    letter_shift_map.insert(Keycode::D, "d".to_string());
+    letter_shift_map.insert(Keycode::E, "e".to_string());
+    letter_shift_map.insert(Keycode::F, "f".to_string());
+    letter_shift_map.insert(Keycode::G, "g".to_string());
+    letter_shift_map.insert(Keycode::H, "h".to_string());
+    letter_shift_map.insert(Keycode::I, "i".to_string());
+    letter_shift_map.insert(Keycode::J, "j".to_string());
+    letter_shift_map.insert(Keycode::K, "k".to_string());
+    letter_shift_map.insert(Keycode::L, "l".to_string());
+    letter_shift_map.insert(Keycode::M, "m".to_string());
+    letter_shift_map.insert(Keycode::N, "n".to_string());
+    letter_shift_map.insert(Keycode::O, "o".to_string());
+    letter_shift_map.insert(Keycode::P, "p".to_string());
+    letter_shift_map.insert(Keycode::Q, "q".to_string());
+    letter_shift_map.insert(Keycode::R, "r".to_string());
+    letter_shift_map.insert(Keycode::S, "s".to_string());
+    letter_shift_map.insert(Keycode::T, "t".to_string());
+    letter_shift_map.insert(Keycode::U, "u".to_string());
+    letter_shift_map.insert(Keycode::V, "v".to_string());
+    letter_shift_map.insert(Keycode::W, "w".to_string());
+    letter_shift_map.insert(Keycode::X, "x".to_string());
+    letter_shift_map.insert(Keycode::Y, "y".to_string());
+    letter_shift_map.insert(Keycode::Z, "z".to_string());
+    
+    let mut char_shift_map: HashMap <Keycode, String> = HashMap::new();
+    char_shift_map.insert(Keycode::Key1, "!".to_string());
+    char_shift_map.insert(Keycode::Key2, "@".to_string());
+    char_shift_map.insert(Keycode::Key3, "#".to_string());
+    char_shift_map.insert(Keycode::Key4, "$".to_string());
+    char_shift_map.insert(Keycode::Key5, "%".to_string());
+    char_shift_map.insert(Keycode::Key6, "^".to_string());
+    char_shift_map.insert(Keycode::Key7, "&".to_string());
+    char_shift_map.insert(Keycode::Key8, "*".to_string());
+    char_shift_map.insert(Keycode::Key9, "(".to_string());
+    char_shift_map.insert(Keycode::Key0, ")".to_string());
+    char_shift_map.insert(Keycode::Apostrophe, "\"".to_string());
+    char_shift_map.insert(Keycode::Semicolon, ":".to_string());
+    char_shift_map.insert(Keycode::LeftBracket, "{".to_string());
+    char_shift_map.insert(Keycode::RightBracket, "}".to_string());
+    char_shift_map.insert(Keycode::Minus, "_".to_string());
+    char_shift_map.insert(Keycode::Equal, "+".to_string());
+    char_shift_map.insert(Keycode::BackSlash, "|".to_string());
+    char_shift_map.insert(Keycode::Comma, "<".to_string());
+    char_shift_map.insert(Keycode::Dot, ">".to_string());
+    char_shift_map.insert(Keycode::Slash, "?".to_string());
+    char_shift_map.insert(Keycode::Grave, "~".to_string());
+
+    let mut symbol_map: HashMap <Keycode, String> = HashMap::new();
+    symbol_map.insert(Keycode::Key1, "1".to_string());
+    symbol_map.insert(Keycode::Key2, "2".to_string());
+    symbol_map.insert(Keycode::Key3, "3".to_string());
+    symbol_map.insert(Keycode::Key4, "4".to_string());
+    symbol_map.insert(Keycode::Key5, "5".to_string());
+    symbol_map.insert(Keycode::Key6, "6".to_string());
+    symbol_map.insert(Keycode::Key7, "7".to_string());
+    symbol_map.insert(Keycode::Key8, "8".to_string());
+    symbol_map.insert(Keycode::Key9, "9".to_string());
+    symbol_map.insert(Keycode::Key0, "0".to_string());
+    symbol_map.insert(Keycode::Apostrophe, "'".to_string());
+    symbol_map.insert(Keycode::Semicolon, ";".to_string());
+    symbol_map.insert(Keycode::LeftBracket, "[".to_string());
+    symbol_map.insert(Keycode::RightBracket, "]".to_string());
+    symbol_map.insert(Keycode::Minus, "-".to_string());
+    symbol_map.insert(Keycode::Equal, "=".to_string());
+    symbol_map.insert(Keycode::BackSlash, "\\".to_string());
+    symbol_map.insert(Keycode::Comma, ",".to_string());
+    symbol_map.insert(Keycode::Dot, ".".to_string());
+    symbol_map.insert(Keycode::Slash, "/".to_string());
+    symbol_map.insert(Keycode::Grave, "`".to_string());
+
     let keys = device_state.get_keys();
-    let mut ctrl_keys: Vec<device_query::Keycode> = Vec::new();
-    let mut data_keys: Vec<device_query::Keycode> = Vec::new();
+    // let mut ctrl_keys: Vec<Keycode> = Vec::new();
+    // let mut data_keys: Vec<Keycode> = Vec::new();
+    let mut ctrl_keys: Vec<String> = vec![];
+    let mut data_keys: Vec<String> =  vec![];
+
+    let mut shift_pressed = false;
 
     for key in &keys {
-        if ctrl_keycodes.contains(key) {
-            ctrl_keys.push(*key);
-        } else {
-            data_keys.push(*key);
+        if shift_keycodes.contains(key) {
+            shift_pressed = true;
+            break;
         }
     }
 
-    let mut key_map: std::collections::HashMap <String, Vec<device_query::Keycode>> = std::collections::HashMap::new();
+    for key in &keys {
+        if shift_keycodes.contains(key) {
+            continue;
+        }
+        if ctrl_keycodes.contains(key) {
+            ctrl_keys.push(key.to_string());
+        } else if shift_pressed && letter_shift_map.contains_key(key) {
+            data_keys.push(key.to_string());
+        } else if shift_pressed && char_shift_map.contains_key(key) {
+            data_keys.push(String::from(&char_shift_map[key]));
+        } else if letter_shift_map.contains_key(key) {
+            data_keys.push(String::from(&letter_shift_map[key]));
+        } else if symbol_map.contains_key(key) {
+            data_keys.push(String::from(&symbol_map[key]));
+        } else {
+            data_keys.push(key.to_string());
+        }
+    }
+
+    let mut key_map: std::collections::HashMap <String, Vec<String>> = std::collections::HashMap::new();
     key_map.insert("ctrl".to_string(), ctrl_keys);
 
     key_map.insert("data".to_string(), data_keys);
 
-    return key_map;
+    key_map
 }
--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,17 @@ mod keylogger;
 use std::io::Write;
 
 fn main() {
-    let mut prev_ctrl_keys: Vec<device_query::Keycode> = Vec::new();
-    let mut prev_data_keys: Vec<device_query::Keycode> = Vec::new();
+    let mut prev_ctrl_keys: Vec<String> = Vec::new();
+    let mut prev_data_keys: Vec<String> = Vec::new();
     const INITIAL_BUFFER_VALUE: Option<String> = None;
     let mut buffer: [Option<String>; 32] = [INITIAL_BUFFER_VALUE; 32];
     let mut buffer_next: usize = 0;
+
+    let delimiters: [String; 3]= [
+        String::from("Space"),
+        String::from("Enter"),
+        String::from("Tab"),
+    ];
     
     loop {
         // Get all keys that were depressed this frame
@@ -19,19 +25,17 @@ fn main() {
         let ctrl_equal = curr_ctrl_keys == prev_ctrl_keys;
         let data_equal = curr_data_keys == prev_data_keys;
 
-        let mut new_ctrl_keys: Vec<device_query::Keycode> = Vec::new();
-        let mut new_data_keys: Vec<device_query::Keycode> = Vec::new();
+        let mut new_ctrl_keys: Vec<String> = Vec::new();
+        let mut new_data_keys: Vec<String> = Vec::new();
         
         if !ctrl_equal && curr_ctrl_keys.len() > 0 {
             // print new characters
             new_ctrl_keys = get_new_keys(&prev_ctrl_keys, &curr_ctrl_keys);
-            println!("{:?}", new_ctrl_keys);
         }
 
         if !data_equal && curr_data_keys.len() > 0 {
             // print new characters
             new_data_keys = get_new_keys(&prev_data_keys, &curr_data_keys);
-            println!("{:?}", new_data_keys);
         }
 
         prev_ctrl_keys = all_keys.get("ctrl").unwrap().to_vec();
@@ -39,27 +43,42 @@ fn main() {
         std::io::stdout().flush().unwrap();
         
         // Add new keys to a buffer
-        for key in new_data_keys {
+        for key in &new_data_keys {
+            if delimiters.contains(key){
+                for index in 0..buffer.len() {
+                    if buffer[index] == None { break }
+                    buffer[index] = None;
+                }
+                buffer_next = 0;
+                break;
+            }
+            
             buffer[buffer_next] = Some(key.to_string());
             buffer_next += 1;
             if buffer_next >= buffer.len() { buffer_next = 0 };
         }
 
-        if buffer_next > 0 {
-            println!("{:?}", buffer);
+        if &new_ctrl_keys.len() > &0 || &new_data_keys.len() > &0 {
+            for key in &buffer {
+                if let Some(value) = key {
+                    print!("{}", value);
+                } else {
+                    print!("{}", String::from("_"));
+                }
+            }
+            println!("");
+            std::io::stdout().flush().unwrap();
+            // println!("{:?}", buffer);
         }
     }
-    // Get all pressed keys
-    // determine which ones are new
-    // add new keys to buffer
     // if end of buffer matches a patter - replace
 }
 
-fn get_new_keys(prev_keys: &Vec<device_query::Keycode>, curr_keys: &Vec<device_query::Keycode>) -> Vec<device_query::Keycode> {
-    let mut new_keys: Vec<device_query::Keycode> = Vec::new();
+fn get_new_keys(prev_keys: &Vec<String>, curr_keys: &Vec<String>) -> Vec<String> {
+    let mut new_keys: Vec<String> = Vec::new();
     for key in curr_keys {
         if !prev_keys.contains(&key) {
-            new_keys.push(*key);
+            new_keys.push(String::from(key));
         }
     }
     new_keys
